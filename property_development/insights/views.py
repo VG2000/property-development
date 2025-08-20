361af4edb62ed801b18250ad9c47edb3cadfd3a3
Vince <47152273+VG2000@users.noreply.github.com>
2025-08-20 11:08:28 +0100

    S3 brochure path

--- property_development/insights/views.py
+++ property_development/insights/views.py
@@ -3,12 +3,8 @@ from django.shortcuts import render
 
 
 def insights_view(request):
-    ctx = {
-        "accommodation_survey_2023": settings.MEDIA_URL + "pdfs/accommodation_survey_2023.pdf",
-        "domestic_stats": settings.MEDIA_URL + "pdfs/day_trips_2024.pdf",
-    }
-    return render(request, "north_wales.html", ctx)
+    return render(request, "north_wales.html")
 
 def conwy_county_view(request):
-    brochure_pdf = settings.MEDIA_URL + "pdfs/Destination-Conwy-Management-Plan-2019-2029.pdf"
+    brochure_pdf = "https://website-public-bucket.s3.eu-west-2.amazonaws.com/wales-development/Destination-Conwy-Management-Plan-2019-2029.pdf"
     return render(request, "conwy_county.html", {"brochure_pdf": brochure_pdf})
